9d4bd98f5dd031c696341fb5f3556f074144e144
Zihanni-33 <126554299+Zihanni-33@users.noreply.github.com>
2023-05-07 18:10:51 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,19 @@
+function sayName(){  
+  var response=prompt("Who is the murder?");
+  if(response=="wife"){
+    alert("Congratulations!You are right");
+  }
+  else{
+    alert("Sorry,you are wrong")
+  }
+}
+
+function sayName2(){
+  var response=prompt("Who is the murder?");
+  if(response=="boyfriend"){
+    alert("Congratulations!You are right");
+  }
+  else{
+    alert("Sorry,you are wrong")
+  }
+}
